91a21cc323dfaee24825747e9880919dc8454a2b
farhanmansoor2015 <farhan.mansoor.qta@gmail.com>
2015-07-27 14:25:55 +0500

    updated..

--- db/migrate/20150715103248_remove_session_to_subjects.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSessionToSubjects < ActiveRecord::Migration
-  def change
-    remove_column :subjects, :session, :date
-  end
-end
--- db/migrate/20150715104507_add_session_to_subjects.rb
+++
@@ -1,5 +0,0 @@
-class AddSessionToSubjects < ActiveRecord::Migration
-  def change
-    add_column :subjects, :session, :date
-  end
-end
